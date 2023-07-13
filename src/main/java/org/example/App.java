e56860e91c7f415fd8283a690266574b34c9413f
semenov-ao <semenov.andrey.2003@gmail.com>
2023-07-13 19:15:17 +0500

    init

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,7 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App 
 {
-
     public static void main( String[] args )
     {
         SpringApplication.run(App.class,args);

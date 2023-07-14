73cfb018fa6df85ae5f3203371964bf72db8accf
semenov-ao <semenov.andrey.2003@gmail.com>
2023-07-14 17:26:14 +0500

    add first test

---
+++ src/test/java/org/example/controller/BankControllerTest.java
@@ -0,0 +1,55 @@
+package org.example.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.domain.Bank;
+import org.example.service.BankService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Collections;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@WebMvcTest(controllers = BankController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+public class BankControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BankService bankService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private final Bank testBank;
+
+    public BankControllerTest() {
+        testBank = new Bank(1L,"Open",123456789L,null);
+    }
+
+    @Test
+    public void BankController_Create_ReturnCreated() throws Exception{
+        ResultActions response = mockMvc
+                .perform(post("/api/bank/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(testBank)));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content()
+                        .json(objectMapper
+                                .writeValueAsString(Collections
+                                        .singletonMap("create",Boolean.TRUE))));
+
+    }
+
+}

79b67da2de32ced9eff85051c409c38c012e5815
Andrey <semenov.andrey.2003@gmail.com>
2023-07-16 02:51:56 +0500

    add unique constraint for Client::name, Bank::bikCode, Bank::name

--- src/main/java/org/example/domain/Bank.java
+++ src/main/java/org/example/domain/Bank.java
@@ -20,10 +20,10 @@ public class Bank {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name="name", nullable = false)
+    @Column(name="name", nullable = false, unique = true)
     private String name;
 
-    @Column(name="bik_code", nullable = false)
+    @Column(name="bik_code", nullable = false, unique = true)
     private Long bikCode;
 
     @OneToMany(mappedBy = "bank",fetch = FetchType.LAZY)
--- src/main/java/org/example/domain/Client.java
+++ src/main/java/org/example/domain/Client.java
@@ -22,7 +22,7 @@ public class Client {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name="name", nullable = false)
+    @Column(name="name", nullable = false, unique = true)
     private String name;
 
     @Column(name="shortname")
